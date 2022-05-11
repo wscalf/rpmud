abea72cf81c009cd969265238be4e76088d5ffed
William Scalf <wscalf@gmail.com>
2022-05-10 22:25:44 -0400

    Refactor to try to break down Command.h, though it didn't go very far

--- server/scripting/Command.h
+++ server/scripting/Command.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "scripting/Segment.h"
+
 #include <string>
 #include <memory>
 #include <map>
@@ -9,40 +11,7 @@
 class Player;
 class Room;
 class MUDObject;
-
-struct ParseResult
-{
-    bool matched;
-    size_t startIndex;
-    std::string_view match;
-
-    static const ParseResult Error;
-};
-
-class Segment
-{
-    public:
-        bool isRequired() const;
-        bool isParameter() const;
-        std::string name() const;
-        std::string errorMsg() const;
-        virtual ParseResult parseNext(std::string_view remaining) const = 0;
-        Segment(std::string name, std::string errMsg);    
-        protected:
-            size_t findNextWhitespace(std::string_view remaining) const;
-            size_t findNextNonwhitespace(std::string_view remaining) const;
-    private:
-        std::string _name;
-        std::string _errMsg;
-};
-
-class SegmentFactory
-{
-    public:
-        static Segment* makeLiteral(std::string text, std::string error = "");
-        static Segment* makeShortText(std::string name, std::string error = "");
-        static Segment* makeFreeText(std::string name, std::string error = "");
-};
+class Segment;
 
 class BoundParameter
 {
